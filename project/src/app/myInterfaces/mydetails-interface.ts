c393b25ff34e53c55531f22cf6af45d3bd828a70
Dhruvil <dhruvilrathod2001@gmail.com>
2021-12-18 11:31:31 +0530

    first commit

--- project/src/app/myInterfaces/mydetails-interface.ts
+++
@@ -1,7 +0,0 @@
-export interface MyDetailsInterFace{
-    A4Name: string;
-    A4Email: string;
-    A4Login: string; 
-    A4ID : number;
-    A4Photo : any;
-}
--- project/src/app/myInterfaces/nation-interface.ts
+++
@@ -1,3 +0,0 @@
-export interface NationFeaturedInterface {
-    nation: string
-}
